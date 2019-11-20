e52309060f32ca8231276b8b13dcf39141865389
angpao <angpao.kittimasak@gmail.com>
2019-11-20 10:42:53 +0700

    add navber home-page add-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddPageComponent } from './components/add-page/add-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'add', component: AddPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddPageComponent } from './components/add-page/add-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NavberComponent } from './components/navber/navber.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPageComponent,
+    HomePageComponent,
+    NavberComponent
   ],
   imports: [
     BrowserModule,
